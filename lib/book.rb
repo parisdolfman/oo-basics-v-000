11a21c40997fcab72e806d67936ce9c4dc83dca7
Paris Dolfman <parisdolfman@gmail.com>
2018-07-17 23:59:24 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
 
 
+class Book  # creates class for Book
+  attr_accessor :author, :page_count, :genre
+  
+  def initialize(title)  # each time a new book is instantiated it allows the title to be passed as an argument
+    @title = title  # argument passed in is set to equal the instance variable @title
+  end
+
+  def title # sets the variable @title to return what is written to the method
+    @title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
